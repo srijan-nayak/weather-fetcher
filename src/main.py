fbfc4598f78f68447a91c4c74f515dadde05968f
Srijan Nayak <srijannayak0@gmail.com>
2019-06-24 11:30:11 +0530

    Get query data for an user entered city name

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("check")
+import requests
+
+while True:
+    city = input("Enter city name: ")
+    try:
+        search_response = requests.get(
+            f"https://www.metaweather.com/api/location/search/?query={city}")
+    except requests.exceptions.ConnectionError:
+        print("Could not connect to the metaweather server!")
