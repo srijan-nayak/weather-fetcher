938ab9281a1099da6dda20fc7957d5332b44022c
Srijan Nayak <srijannayak0@gmail.com>
2019-06-24 13:11:47 +0530

    Add get_valid_city function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 import requests
 
-while True:
+
+def get_valid_city() -> str:
     city = input("Enter city name: ")
-    try:
-        search_response = requests.get(
-            f"https://www.metaweather.com/api/location/search/?query={city}")
-        if search_response.text == "[]":
-            print("City not found!")
-            continue
-        location_data = search_response.json()
-        woeid = location_data[0]["woeid"]
-    except requests.exceptions.ConnectionError:
-        print("Could not connect to the metaweather server!")
+    search_response = requests.get(
+        f"https://www.metaweather.com/api/location/search/?query={city}")
+    if search_response.text == "[]":
+        print("City not found!")
+        city = get_valid_city()
+    return city
+
+
+try:
+    city = get_valid_city()
+    search_response = requests.get(
+        f"https://www.metaweather.com/api/location/search/?query={city}")
+    location_data = search_response.json()
+    woeid = location_data[0]["woeid"]
+except requests.exceptions.ConnectionError:
+    print("Could not connect to the metaweather server!")
