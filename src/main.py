95ffe8acd9c9dfb06d146b7368e164f6b9bf2a3e
Srijan Nayak <srijannayak0@gmail.com>
2019-06-24 17:06:35 +0530

    Extract get_woeid function

--- src/main.py
+++ src/main.py
@@ -11,11 +11,16 @@ def get_valid_city() -> str:
     return city
 
 
-try:
-    city = get_valid_city()
+def get_woeid(city: str) -> str:
     search_response = requests.get(
         f"https://www.metaweather.com/api/location/search/?query={city}")
     location_data = search_response.json()
     woeid = location_data[0]["woeid"]
-except requests.exceptions.ConnectionError:
-    print("Could not connect to the metaweather server!")
+    return woeid
+
+
+try:
+    city = get_valid_city()
+    woeid = get_woeid(city)
+except requests.ConnectionError:
+    print("Could not connect to the server!")
