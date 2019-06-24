5bc911bb76f2d799521a1d24791d2bf047bf1797
Srijan Nayak <srijannayak0@gmail.com>
2019-06-24 12:30:48 +0530

    Check if city is not found in search

--- src/main.py
+++ src/main.py
@@ -5,5 +5,8 @@ while True:
     try:
         search_response = requests.get(
             f"https://www.metaweather.com/api/location/search/?query={city}")
+        if search_response.text == "[]":
+            print("City not found!")
+            continue
     except requests.exceptions.ConnectionError:
         print("Could not connect to the metaweather server!")
