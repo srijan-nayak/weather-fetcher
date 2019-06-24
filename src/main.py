d33d434abeb80ceb2fd09c4fa4c148ff31713e74
Srijan Nayak <srijannayak0@gmail.com>
2019-06-24 17:25:51 +0530

    Rename all search_response to request_response

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ import requests
 
 def get_valid_city() -> str:
     city = input("Enter city name: ")
-    search_response = requests.get(
+    request_response = requests.get(
         f"https://www.metaweather.com/api/location/search/?query={city}")
-    if search_response.text == "[]":
+    if request_response.text == "[]":
         print("City not found!")
         city = get_valid_city()
     return city
 
 
 def get_woeid(city: str) -> str:
-    search_response = requests.get(
+    request_response = requests.get(
         f"https://www.metaweather.com/api/location/search/?query={city}")
-    location_data = search_response.json()
+    location_data = request_response.json()
     woeid = location_data[0]["woeid"]
     return woeid
 
